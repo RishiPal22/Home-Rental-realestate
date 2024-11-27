f91338854eab01c90c72e5adb3f118a8921a0946
RishiPal22 <rishipallv22@gmail.com>
2024-11-27 12:45:16 +0530

    Mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config() 
+mongoose.connect(process.env.MONGOSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("Failed to connect to MongoDB", err));
 
 app.listen(3000,(req,res)=>{
     console.log('Server is running on port 3000')
-})
+});
