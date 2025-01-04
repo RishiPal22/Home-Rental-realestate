b428c5eff225dbac3fd8aa62a0e7c31ea317e251
RishiPal22 <rishipallv22@gmail.com>
2025-01-04 16:10:50 +0530

    Upload Image Functionality

--- client/src/pages/Createlisting.jsx
+++ client/src/pages/Createlisting.jsx
@@ -1,6 +1,76 @@
-import React from 'react'
+import { set } from 'mongoose'
+import React, { useState } from 'react'
 
 export default function Createlisting() {
+  const [file, setFile] = useState([])
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  })
+
+  const [imageUploadError, setImageUploadError] = useState(false)
+  console.log(formData)
+
+  const handleImageSubmit = (e) => {
+
+    e.preventDefault()
+    const Promises = []
+    try {
+      if (file.length > 0 && file.length + formData.imageUrls.length < 7) {
+
+        for (let i = 0; i < file.length; i++) {
+          Promises.push(storeImage(file[i]))
+        }
+
+        Promise.all(Promises).then((urls) => {
+          setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls) })
+
+        })
+        setImageUploadError(false)
+        console.log(formData)
+
+      }
+      else {
+        setImageUploadError('Please select between 1 and 6 images')
+        console.log('Please select between 1 and 6 images')
+      }
+      console.log(file)
+    } catch (err) {
+      setImageUploadError("Upload failure because of some internal issue")
+      console.log(err)
+    }
+  }
+
+
+  const storeImage = async (file) => {
+    return new Promise(async (resolve, reject) => {
+      const data = new FormData()
+      data.append('file', file)
+      data.append('upload_preset', 'first_time_using_cloudinary')
+      data.append('cloud_name', 'dkk6lmgxq')
+
+      try {
+        const res = await fetch('https://api.cloudinary.com/v1_1/dkk6lmgxq/image/upload', {
+          method: "POST",
+          body: data
+        })
+
+        if (!res.ok) {
+          throw new Error('File upload failed')
+        }
+
+        const uploadedImageUrl = await res.json()
+        console.log(uploadedImageUrl)
+        resolve(uploadedImageUrl.url)
+      } catch (err) {
+        reject(err.message)
+        console.log(err.message)
+        // return uploadedImageUrl.url
+      }
+    });
+
+  }
+
+
   return (<>
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl text-center p-2'> Create Listing</h1>
@@ -71,12 +141,25 @@ export default function Createlisting() {
         <div className='flex flex-col flex-1'>
           <p className='text-lg'>Images: <span className='ml-2 text-sm text-gray-600'>The first image will be the cover</span></p>
           <div className='p-2 m-2'>
-            <input type='file' id='images' accept='image/*'/>
-            <button className='rounded-lg uppercase text-green-600 border-green-600  p-1 border w-auto'> Upload</button>
+
+            <input onChange={(e) => setFile(e.target.files)} type='file' id='images' accept='image/*' multiple />
+            <button onClick={handleImageSubmit}
+              className='rounded-lg uppercase text-green-600 border-green-600  p-1 border w-auto'> Upload</button>
+            {imageUploadError && <p className='text-red-500'>{imageUploadError}</p>}
+            <div>
+
+              {formData.imageUrls.length > 0 && formData.imageUrls.map((url) => (
+                <div className='flex justify-between p-2'>
+                  <img src={url} alt='Image Listing' className='sm:h-20' />
+                  <button className='p-2 text-red-600'>Delete</button>
+
+                </div>
+              ))}
+            </div>
           </div>
           <button className='p-2 bg-slate-700 text-white uppercase'>Create Listing </button>
         </div>
-        
+
       </form>
     </main>
   </>
